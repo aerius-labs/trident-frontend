d764f75941ff5e6dc17cea3fffe157b04a967596
manavbabber <manavfazilka@gmail.com>
2024-07-08 16:04:55 +0530

    feat: added wallet info table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import PageContainer from '@/components/pageContainer';
 import AuroConnect from '@/hooks/AuroConnect';
 
 import PlaceOrderSection from '@/components/placeOrderSection';
+import { WalletStats } from '@/components/walletStats';
 export default function Home() {
   return (
     <PageContainer>
@@ -13,15 +14,13 @@ export default function Home() {
       <main className='flex flex-col gap-4'>
         <Container className='col-span-12 h-[74px] rounded-2xl bg-secondary'></Container>
         <Container>
-          <div className='col-span-3 flex h-auto min-h-[770px] flex-col items-center justify-center rounded-2xl bg-secondary'>
+          <div className='col-span-3 flex min-h-[770px] flex-col items-center justify-center rounded-2xl bg-secondary'>
             <PlaceOrderSection />
           </div>
-          <div className='col-span-3 h-auto min-h-[770px] rounded-2xl bg-secondary'></div>
-          <div className='col-span-6 flex h-auto min-h-[770px] flex-col rounded-2xl bg-secondary'>
-            <div className='h-[404px]'>
-              <TradingViewWidget />
-            </div>
-            <div></div>
+          <div className='col-span-3 min-h-[770px] rounded-2xl bg-secondary'></div>
+          <div className='col-span-6 flex max-h-screen min-h-[770px] flex-col rounded-2xl bg-secondary'>
+            <TradingViewWidget />
+            <WalletStats />
           </div>
         </Container>
       </main>
