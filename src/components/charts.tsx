4c668c212104dcecf672fc962fdb86d25aad4981
Yogesh Shahi <yogeshshahi120@gmail.com>
2024-07-08 12:13:37 +0530

    feat: added charts to layout

--- src/components/charts.tsx
+++ src/components/charts.tsx
@@ -3,51 +3,37 @@
 import React, { memo, useEffect, useRef } from 'react';
 
 function TradingViewWidget() {
-  const container = useRef<HTMLDivElement>(null);
+  const container = useRef();
 
   useEffect(() => {
-    if (container.current) {
-      const script = document.createElement('script');
-      script.src =
-        'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
-      script.type = 'text/javascript';
-      script.async = true;
-      script.innerHTML = `
+    const script = document.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
         {
-          "autosize": true,
-          "symbol": "NASDAQ:AAPL",
+          "width": "638",
+          "height": "404",
+          "symbol": "BINANCE:MINAUSDT",
           "interval": "D",
           "timezone": "Etc/UTC",
           "theme": "dark",
           "style": "1",
           "locale": "en",
+          "backgroundColor": "rgba(0, 0, 0, 1)",
+          "hide_legend": true,
           "allow_symbol_change": true,
+          "details": true,
           "calendar": false,
           "support_host": "https://www.tradingview.com"
         }`;
-      container.current.appendChild(script);
-    }
+    container.current.appendChild(script);
   }, []);
 
   return (
-    <div
-      className='tradingview-widget-container'
-      ref={container}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <div
-        className='tradingview-widget-container__widget'
-        style={{ height: 'calc(100% - 32px)', width: '100%' }}
-      ></div>
-      <div className='tradingview-widget-copyright'>
-        <a
-          href='https://www.tradingview.com/'
-          rel='noopener nofollow noreferrer'
-          target='_blank'
-        >
-          <span className='blue-text'>Track all markets on TradingView</span>
-        </a>
-      </div>
+    <div className='tradingview-widget-container' ref={container}>
+      <div className='tradingview-widget-container__widget'></div>
     </div>
   );
 }
